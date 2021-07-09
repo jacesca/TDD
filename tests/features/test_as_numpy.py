43f8ab44dde5e32dce2a4bb8be2af67782364dd4
Jacqueline de Cortez <jacesca@gmail.com>
2021-07-09 11:58:51 -0600

    Fixing features/test_as_numpy.py (2nd)

--- tests/features/test_as_numpy.py
+++ tests/features/test_as_numpy.py
@@ -6,19 +6,17 @@ import os
 
 @pytest.fixture
 def clean_data_file():
-    file_path = "clean.txt"
-    with open(file_path, "w") as f:
-        f.write("201\t305671\n7892\t298140\n501\t738293\n")
-    yield file_path
-    os.remove(file_path)
+	file_path = "clean.txt"
+	with open(file_path, "w") as f:
+		f.write("201\t305671\n7892\t298140\n501\t738293\n")
+	yield file_path
+	os.remove(file_path)
 
 class TestGetDataAsNumpyArray(object):
-    def test_on_clean_file(self):
-        expected = np.array([[2081.0, 314942.0],
-                             [1059.0, 186606.0],
-                             [1148.0, 206186.0]
-                            ])
-        actual = get_data_as_numpy_array(clean_data_file, num_columns=2)
+    def test_on_clean_file(self, clean_data_file):
+        expected = np.array([[201.0, 305671.0], [7892.0, 298140.0], [501.0, 738293.0]])
+        actual = get_data_as_numpy_array(clean_data_file, 2)
+		
         message = "Expected return value: {0}, Actual return value: {1}".format(expected, actual)
         assert (actual == expected).all()
     
