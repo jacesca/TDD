b082160208e09c5cb7c90ad13fde96e448d80ff8
Jacqueline de Cortez <jacesca@gmail.com>
2021-07-09 20:07:42 -0600

    features/test_as_numpy.py fixture

--- tests/features/test_as_numpy.py
+++ tests/features/test_as_numpy.py
@@ -12,11 +12,23 @@ def clean_data_file():
 	yield file_path
 	os.remove(file_path)
 
+@pytest.fixture
+def empty_file():
+    file_path = "empty.txt"
+    open(file_path, "w").close()
+    yield file_path
+    os.remove(file_path)
+    
+
 class TestGetDataAsNumpyArray(object):
     def test_on_clean_file(self, clean_data_file):
         expected = np.array([[201.0, 305671.0], [7892.0, 298140.0], [501.0, 738293.0]])
         actual = get_data_as_numpy_array(clean_data_file, 2)
-		
+        
         message = "Expected return value: {0}, Actual return value: {1}".format(expected, actual)
         assert (actual == expected).all()
-    
+        
+    def test_on_empty_file(self, empty_file):
+        expected = np.empty((0, 2))
+        actual = get_data_as_numpy_array(empty_file, 2)
+        assert actual == pytest.approx(expected), "Expected: {0}, Actual: {1}".format(expected, actual)
