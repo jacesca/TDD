f4b04f3cb61b31c011c9ec8367f8e0ff87b31439
Jacqueline de Cortez <jacesca@gmail.com>
2021-07-10 08:30:47 -0600

    adding more test class to tests/models/test_train.py

--- tests/models/test_train.py
+++ tests/models/test_train.py
@@ -1,7 +1,8 @@
 import pytest
 import numpy as np
+from math import cos, pi, sin
 
-from models.train import split_into_training_and_testing_sets
+from models.train import split_into_training_and_testing_sets, train_model, model_test
 
 # Declare the test class
 class TestSplitIntoTrainingAndTestingSets(object):
@@ -43,4 +44,53 @@ class TestSplitIntoTrainingAndTestingSets(object):
     
         # Check if ValueError contains correct message
         assert exception_info.match("Argument data_array must be two dimensional. Got 1 dimensional array instead!")
+
+class TestTrainModel(object):
+    def test_on_linear_data(self):
+        test_argument = np.array([[1.0, 3.0],
+                                  [2.0, 5.0],
+                                  [3.0, 7.0]])
+        expected_slope = 2.0
+        expected_intercept = 1.0
     
+        slope, intercept = train_model(test_argument)
+    
+        assert slope == pytest.approx(expected_slope)
+        assert intercept == pytest.approx( expected_intercept )
+    
+    def test_on_positively_correlated_data(self):
+        test_argument = np.array([[1.0, 4.0], [2.0, 4.0],
+                                  [3.0, 9.0], [4.0, 10.0],
+                                  [5.0, 7.0], [6.0, 13.0],])
+        slope, intercept = train_model(test_argument)
+        assert slope > 0
+
+
+class TestModelTest(object):
+    def test_on_perfect_fit(self):
+        # Assign to a NumPy array containing a linear testing set
+        test_argument = np.array([[1.0, 3.0], [2.0, 5.0], [3.0, 7.0]])
+        # Fill in with the expected value of r^2 in the case of perfect fit
+        expected = 1.0
+        # Fill in with the slope and intercept of the model
+        slope = (test_argument[-1,1]-test_argument[0,1])/(test_argument[-1,0]-test_argument[0,0])
+        intercept = test_argument[0,1] - (slope * test_argument[0, 0]) 
+        actual = model_test(test_argument, slope=slope, intercept=intercept)
+        # Complete the assert statement
+        assert actual == pytest.approx(expected), "Expected: {0}, Actual: {1}".format(expected, actual)
+    
+    def test_on_circular_data(self):
+        theta = pi/4.0
+        # Assign to a NumPy array holding the circular testing data
+        test_argument = np.array([[1.0, 0.0], [cos(theta), sin(theta)],
+                                  [0.0, 1.0],
+                                  [cos(3 * theta), sin(3 * theta)],
+                                  [-1.0, 0.0],
+                                  [cos(5 * theta), sin(5 * theta)],
+                                  [0.0, -1.0],
+                                  [cos(7 * theta), sin(7 * theta)]])
+                                  
+        # Fill in with the slope and intercept of the straight line
+        actual = model_test(test_argument, slope=0.0, intercept=0.0)
+        # Complete the assert statement
+        assert actual == pytest.approx(0.0)
